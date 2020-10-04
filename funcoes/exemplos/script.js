4bbbc89b4a9c9c660b1163a05ac2749903f0f347
Natália Luiza <natalialuizas@live.com>
2020-10-04 10:16:20 -0300

    exemplos de funções

--- funcoes/exemplos/script.js
+++ funcoes/exemplos/script.js
@@ -6,20 +6,30 @@
 /*
   Funções anônimas são aquelas em que o nome da função não é definido
 */
-
-
+// function(){
+//  console.log('estou executando alguma coisa')
+// }
 
 // Funções Literal
-
+function mostrarMensagem() {
+  console.log("Seja bem vindo");
+}
 
 // Váriaveis podem receber funções
+const soma = function (a, b) {
+  return a + b;
+};
 
+console.log(soma(10, 10));
 
 // Arrow Function
-
+const sub = (a, b) => {
+  return a - b;
+};
 
 // Parênteses executa a função
-
+console.log(soma(10, 20));
+console.log(soma(10, 10));
 
 /*
   Parâmetros e argumentos
@@ -27,15 +37,25 @@
   Ao executar uma função, você pode passar argumentos.
 */
 
+const gorjetaDoGarcom = (valorJantar, porcetagem) => {
+  const gorjeta = valorJantar * (porcetagem / 100);
+  return gorjeta;
+};
 
+console.log(gorjetaDoGarcom(100, 10));
+console.log(gorjetaDoGarcom(250, 15));
 
 /*
     Pode ou não retornar um valor
     Quando não definimos o return, ela irá retornar undefined. O código interno da função é executado normalmente, independente de existir valor de return ou não.
 */
 
+const imc = (peso, altura) => {
+  const imc = peso / altura ** 2;
+  console.log(imc);
+};
 
-
+console.log(imc(90, 1.65));
 
 /* 
   Valores retornados 
@@ -43,7 +63,23 @@
   Cuidado, retornar diferentes tipos de dados na mesma função não é uma boa ideia.
  */
 
-
+const pagamento = (tipo, total, valorRecebido) => {
+  switch (tipo) {
+    case "cheque":
+      return "Não trabalhamos com cheque";
+      break;
+    case "cartao":
+      return true;
+      break;
+    case "moeda":
+      return false;
+    case "dinheiro":
+      troco(total, valorRecebido);
+      break;
+    default:
+      return "Opção invalida";
+  }
+};
 
 /* 
   Escopo
@@ -51,15 +87,32 @@
   Funções conseguem acessar variáveis que foram criadas no contexto pai
 */
 
+// escopo global
+let calcular = 0;
 
+const multi = (x, z) => {
+  // escopo local
+  let calcular = x * z;
+  console.log(calcular);
+};
 
+console.log(multi(10, 10));
+
+console.log(calcular);
 
 /* 
   Hoisting
   Antes de executar uma função, o JS 'move' todas as funções declaradas para a memória
 */
+console.log(comidaFavorita("pudim"));
 
+// function comidaFavorita(comida){
+//   return `sua comida favorita é ${comida}`
+// }
 
+const comidaFavorita = (comida) => {
+  return `sua comida favorita é ${comida}`;
+};
 
 /*
     Funções
